92461043049c2dbb8edfe57ca41036c74928ca30
pro101noob <baryshnikvlad@gmail.com>
2018-10-05 15:55:16 +0100

    123
    123

--- secs_days.c
+++ secs_days.c
@@ -2,10 +2,10 @@
 #include<math.h>
 
 int main() {
-  int k;
+  int k = 0;
   scanf("%d", &k);
-  printf("years:", "%.d", k/365);
-  printf("weeks:", "%.d", k%365/7);
-  printf("days:", "%.d", k%365%7);
+  printf("years: %.d\n", k/365);
+  printf("weeks: %.d\n", k%365/7);
+  printf("days: %.d\n", k%365%7);
   return 0;
 }
